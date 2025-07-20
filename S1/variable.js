f40266eb125ac7f7c6bf0edc2c5167ac2c4d532c
AydinZarifie <aydinzarifieaszo@gmail.com>
2025-07-20 23:24:12 +0330

    s1

---
+++ S1/variable.js
@@ -0,0 +1,54 @@
+
+//string
+const firstName = "20";
+
+var lastName = "mohammadi";
+
+let cityName = "tabriz";
+
+var lastName = "zarifie"
+
+//boolean
+var isExist = true;
+    isExist = false;
+
+//number
+var age = 20;
+
+let x = 20;
+let y = 10;
+
+let result = 22%10;
+
+
+//array
+var session = ["blue" , "red" , "green", "white"];
+
+
+const randomSelect = Math.floor(Math.random());//انتخب روندوم
+
+//Math.floor() به کف گرد میکند
+//Math.ceil()به سقف گرد میکند      
+
+//Math.floor(Math.random()* (max-min) + min) فرومول استفاده از رندوم به صورت بازه ای 
+
+
+
+console.log(session);
+
+
+session.reverse(); //برعکس میکنه
+session.shift();//یدونه از اولین ایندکس به جلو شیفت میده
+session.pop();//از آخر یدونه حذف میکنه
+session.push("black") //به آخر هرمقدار ورودی که بدیم یدونه اضافه میکنه
+session.indexOf("black" ,0)//برای ما ایندکس ورودی که میدیم بر میگیردونه فیلد دومش مشخص میکنه از کدوم اینکس شروع کنه به جست و جو
+session.includes("green")//مشخص میکنه که آیا مقدار ورودی در آرایه هست یا نیست
+session.length()//طول آرایه رو برامون برمیگردونه
+session.splice(1);//ایندکس ورودی را حذف میکند
+
+console.log();//با این دستور میتونیم خروجی بگیریم
+
+
+
+
+
